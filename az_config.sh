1fee85ed0293056c9458e9da6534aef1774913e9
xzizka <9096952+xzizka@users.noreply.github.com>
2022-08-05 18:30:50 +0200

    install az

--- az_config.sh
+++ az_config.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 # This file configures deloyment of Azure cli to manage secrets from Vault.
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo dnf install -y https://packages.microsoft.com/config/rhel/8/packages-microsoft-prod.rpm
+echo -e "[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo
+
+sudo dnf install azure-cli -y
+# Azure Vault
+az login --msi
